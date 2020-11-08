0251c27bafea64373f12d1bd2bd0f5006758c173
jpbrab0 <jpresende502@gmail.com>
2020-11-08 11:42:28 -0300

    feature: Add copy paste

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
 		var borderRadiusValueRightDown = $("#border-radius-value-right-down").val()
 		var borderRadiusRalueLeftDown = $("#border-radius-value-left-down").val()
 		$("#div-border-radius").css('border-radius', `${borderRadiusValueLeft}% ${borderRadiusValueRight}% ${borderRadiusValueRightDown}% ${borderRadiusRalueLeftDown}%`)
-		$("#result-border-radius").text(`border-radius:${borderRadiusValueLeft}% ${borderRadiusValueRight}% ${borderRadiusValueRightDown}% ${borderRadiusRalueLeftDown}%`)
+		$("#input-value-border-radius").val(`border-radius:${borderRadiusValueLeft}% ${borderRadiusValueRight}% ${borderRadiusValueRightDown}% ${borderRadiusRalueLeftDown}%`)
 	})
 })
+
+function copyCss(){
+	var copyText = document.querySelector("#input-value-border-radius");
+	copyText.select();
+  	copyText.setSelectionRange(0, 99999)
+	document.execCommand("copy");
+	alert("Code now is in your copypaste!")
+}
+$("#copy-code").click(copyCss)
